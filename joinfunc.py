5ec26861de4a9747aa7074378057252cae193e5a
Shradha Mohanty <82564374+Sh-glitch-art@users.noreply.github.com>
2022-11-27 10:36:59 +0530

    Add files via upload

---
+++ joinfunc.py
@@ -0,0 +1,7 @@
+lis = ["John", "cena", "Randy", "orton", "Sheamus", "Khali", "jinder mahal"]
+
+# for item in lis:
+#     print(item,"and", end = " ")
+
+a = "and".join(lis)
+print(a, "other wwe superstars")
